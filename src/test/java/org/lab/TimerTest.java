abf586a1a9d21400a0e7dfc8639d66f8f8da5fe4
Maksim Shirma <maksimshirma@mail.ru>
2023-10-31 10:13:54 +0300

    added unit tests for Timer

---
+++ src/test/java/org/lab/TimerTest.java
@@ -0,0 +1,106 @@
+package org.lab;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class TimerTest {
+    private final int repetitions;
+    private final Timer timer;
+
+    public TimerTest() {
+        this.repetitions = 10000;
+        this.timer = new Timer(this.repetitions);
+    }
+
+    @Test
+    public void measureAddTime() {
+        List<Integer> arrayList = new ArrayList<>();
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < this.repetitions; i++) {
+            arrayList.add(i);
+        }
+        long endTime = System.currentTimeMillis();
+        long arrayTime = endTime - startTime;
+
+        List<Integer> linkedList = new LinkedList<>();
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i < this.repetitions; i++) {
+            linkedList.add(i);
+        }
+        endTime = System.currentTimeMillis();
+        long linkedTime = endTime - startTime;
+
+        long measuredArrayTime = timer.measureAddTime(new ArrayList<>());
+        long measuredLinkedTime = timer.measureAddTime(new LinkedList<>());
+
+        assertTrue(measuredArrayTime >= 0 && arrayTime - 2 <= measuredArrayTime && arrayTime + 2 >= measuredArrayTime);
+        assertTrue(measuredLinkedTime >= 0 && linkedTime - 2 <= measuredLinkedTime && linkedTime + 2 >= measuredLinkedTime);
+    }
+
+    @Test
+    public void measureGetTime() {
+        List<Integer> arrayList = new ArrayList<>();
+        for (int i = 0; i < this.repetitions; i++) {
+            arrayList.add(i);
+        }
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < this.repetitions; i++) {
+            arrayList.get(i);
+        }
+        long endTime = System.currentTimeMillis();
+        long arrayTime = endTime - startTime;
+
+        List<Integer> linkedList = new LinkedList<>();
+        for (int i = 0; i < this.repetitions; i++) {
+            linkedList.add(i);
+        }
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i < this.repetitions; i++) {
+            linkedList.get(i);
+        }
+        endTime = System.currentTimeMillis();
+        long linkedTime = endTime - startTime;
+
+        long measuredArrayTime = timer.measureGetTime(new ArrayList<>());
+        long measuredLinkedTime = timer.measureGetTime(new LinkedList<>());
+
+        assertTrue(measuredArrayTime >= 0 && arrayTime - 4 <= measuredArrayTime && arrayTime + 4 >= measuredArrayTime);
+        assertTrue(measuredLinkedTime >= 0 && linkedTime - 4 <= measuredLinkedTime && linkedTime + 4 >= measuredLinkedTime);
+    }
+
+    @Test
+    public void measureDeleteTime() {
+        List<Integer> arrayList = new ArrayList<>();
+        for (int i = 0; i < this.repetitions; i++) {
+            arrayList.add(i);
+        }
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < this.repetitions; i++) {
+            arrayList.remove(0);
+        }
+        long endTime = System.currentTimeMillis();
+        long arrayTime = endTime - startTime;
+
+        List<Integer> linkedList = new LinkedList<>();
+        for (int i = 0; i < this.repetitions; i++) {
+            linkedList.add(i);
+        }
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i < this.repetitions; i++) {
+            linkedList.remove(0);
+        }
+        endTime = System.currentTimeMillis();
+        long linkedTime = endTime - startTime;
+
+        long measuredArrayTime = timer.measureDeleteTime(new ArrayList<>());
+        long measuredLinkedTime = timer.measureDeleteTime(new LinkedList<>());
+
+        assertTrue(measuredArrayTime >= 0 && arrayTime - 2 <= measuredArrayTime && arrayTime + 2 >= measuredArrayTime);
+        assertTrue(measuredLinkedTime >= 0 && linkedTime - 2 <= measuredLinkedTime && linkedTime + 2 >= measuredLinkedTime);
+    }
+}
